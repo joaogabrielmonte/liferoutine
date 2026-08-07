065681c717c129e183ed06958b34684ebff1e9bc
Gabriel <joaogabrielmonteg41@gmail.com>
2026-08-07 16:33:15 -0300

    fix: import TouchableOpacity in HeatmapCalendar

--- src/components/molecules/HeatmapCalendar/index.tsx
+++ src/components/molecules/HeatmapCalendar/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { useTheme } from '@/hooks/useTheme';
 import { AppText } from '@/components/atoms/AppText';
 import { Radius, Spacing } from '@/constants/theme';
